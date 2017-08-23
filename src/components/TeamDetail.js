f64a667dbda7734513522a09c81f6955df4f9e21
Evgenii Utkin <thewulf7@gmail.com>
2017-08-23 07:36:03 +0200

    image implementation

--- src/components/TeamDetail.js
+++ src/components/TeamDetail.js
@@ -1,20 +1,36 @@
-import React, { Component } from 'react';
-import { Text } from 'react-native';
+import React from 'react';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const TeamDetail = (props) => (
-    <Card>
-        <CardSection>
-            <Text>{props.team.team_name}</Text>
-        </CardSection>
-        <CardSection>
-            <Text>{props.team.team_name}</Text>
-        </CardSection>
-        <CardSection>
-            <Text>{props.team.team_name}</Text>
-        </CardSection>
-    </Card>
-);
+const TeamDetail = ({ team }) => {
+    const { team_name } = team;
+    const { thumbnailStyle, headerContentStyle } = styles;
+
+    return (
+        <Card>
+            <CardSection>
+                <View>
+                    <Image style={thumbnailStyle} source={{ uri: 'http://www.bitcoingambling.net/wp-content/uploads/2015/05/Y9cG2XlxsaTUP1wGMHiOGjpDSGmcczHS3EABEN8s_ORvUk4n57Z_XQwuC0GAYX-O91Aw300.png'}} />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text>{team_name}</Text>
+                    <Text>{team_name}</Text>
+                </View>
+            </CardSection>
+        </Card>
+    );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50,
+    },
+};
 
 export default TeamDetail;
