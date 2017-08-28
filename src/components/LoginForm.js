03e5cc91710faeca3e5969112324e980e0bacdab
Evgenii Utkin <thewulf7@gmail.com>
2017-08-28 22:28:15 +0200

    new eslint - lesson 70

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import {Input, Button, Card, CardSection} from './common';
 
 class LoginForm extends Component {
     state = {
         email: '',
-        password: ''
+        password: '',
+        error: ''
     };
+
+    onButtonPress() {
+        const {email, password} = this.state;
+
+        this.setState({ error: '' })
+
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .catch(() => {
+                firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(email, password)
+                    .catch(() => {
+                        this.setState({error: 'Auth failed'});
+                    });
+            });
+    }
 
     render() {
         return (
@@ -25,11 +46,27 @@ class LoginForm extends Component {
                         value={this.state.password}
                         onChangeText={(password) => this.setState({password})}/>
                 </CardSection>
+                <Text style={styles.errorTextStyle}>
+                    {this.state.error}
+                </Text>
                 <CardSection>
-                    <Button>Login</Button>
+                    <Button
+                        onPress={this
+                        .onButtonPress
+                        .bind(this)}>
+                        Login
+                    </Button>
                 </CardSection>
             </Card>
         );
+    }
+}
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
     }
 }
 
