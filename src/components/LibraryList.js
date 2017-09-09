ad83d6beff71dd36a04f6892dfe336ae53677960
Evgenii Utkin <thewulf7@gmail.com>
2017-09-09 10:06:42 +0200

    88

---
+++ src/components/LibraryList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class LibraryList extends Component {
+    state = {  }
+    render() {
+        return (
+            
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return { libraries: state.libraries };
+};
+
+export default connect(mapStateToProps)(LibraryList);
