74072d9fecb951a63dfb0e50088711dd3e2bdc31
Evgenii Utkin <thewulf7@gmail.com>
2017-09-17 18:19:05 +0200

    components add

--- src/app.js
+++ src/app.js
@@ -5,27 +5,27 @@ import { createStore } from "redux";
 import reducers from "./reducers";
 
 class App extends Component {
-  componentWillMount() {
-    const config = {
-      apiKey: "AIzaSyC1bhr5feSETBGmjLf6qDQgWMusa0R77oQ",
-      authDomain: "test-auth-6902b.firebaseapp.com",
-      databaseURL: "https://test-auth-6902b.firebaseio.com",
-      projectId: "test-auth-6902b",
-      storageBucket: "test-auth-6902b.appspot.com",
-      messagingSenderId: "978771896470"
-    };
-    firebase.initializeApp(config);
-  }
+    componentWillMount() {
+        const config = {
+            apiKey: "AIzaSyC1bhr5feSETBGmjLf6qDQgWMusa0R77oQ",
+            authDomain: "test-auth-6902b.firebaseapp.com",
+            databaseURL: "https://test-auth-6902b.firebaseio.com",
+            projectId: "test-auth-6902b",
+            storageBucket: "test-auth-6902b.appspot.com",
+            messagingSenderId: "978771896470",
+        };
+        firebase.initializeApp(config);
+    }
 
-  render() {
-    return (
-      <Provider store={createStore(reducers)}>
-        <View>
-          <Text> Hello ! </Text>
-        </View>
-      </Provider>
-    );
-  }
+    render() {
+        return (
+            <Provider store={createStore(reducers)}>
+                <View>
+                    <Text> Hello ! </Text>
+                </View>
+            </Provider>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/common/CardSection.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+const CardSection = (props) => (
+    <View style={styles.containerStyle}>
+        {props.children}
+    </View>
+);
+
+const styles = {
+  containerStyle: {
+    borderWidth: 1,
+    padding: 5,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    borderColor: '#ddd',
+    position: 'relative',
+  },
+};
+
+export { CardSection };
